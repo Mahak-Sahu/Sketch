38d0aece6228d75422efb0041724300198d899cd
Mahak-Sahu <mahaksahu808@gmail.com>
2024-12-25 15:33:34 +0530

    app.py

--- projet-folder/app.py
+++ projet-folder/app.py
@@ -26,7 +26,7 @@ def convert_to_sketch(image_path):
 
     grey_filter = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     invert = cv2.bitwise_not(grey_filter)
-    blur = cv2.GaussianBlur(invert, (51, 51), 0)
+    blur = cv2.GaussianBlur(invert, (71, 71), 0)
     invertedblur = cv2.bitwise_not(blur)
     sketch_filter = cv2.divide(grey_filter, invertedblur, scale=256.0)
     output_path = os.path.join(OUTPUT_FOLDER, 'output.png')
